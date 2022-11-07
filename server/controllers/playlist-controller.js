e52818c60910264508ed8bd58da62167b72e5582
Muhammad Usman Rehan <90567248+Usman54321@users.noreply.github.com>
2022-11-07 18:02:01 -0500

    Deleting lists also deletes it from User's playlists

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -65,8 +65,14 @@ deletePlaylist = async (req, res) => {
                 if (user._id == req.userId) {
                     //console.log("correct user!");
                     Playlist.findOneAndDelete({ _id: req.params.id }, () => {
-                        return res.status(200).json({success: true})
+                        return res.status(200).json({ success: true })
                     }).catch(err => console.log(err))
+                    // Delete the playlist from the user's list of playlists
+                    let index = user.playlists.indexOf(req.params.id);
+                    if (index > -1) {
+                        user.playlists.splice(index, 1);
+                    }
+                    user.save();
                 }
                 else {
                     console.log("incorrect user!");
